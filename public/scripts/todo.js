fb29663461afa55133875a590d535bbca0d04c2f
ian bevel & jess engel <student@epicodus.com>
2015-07-29 16:10:19 -0700

    allow user to add a task and description

---
+++ public/scripts/todo.js
@@ -0,0 +1,64 @@
+var TodoBox = React.createClass({
+  getInitialState: function() {
+    return {
+      data:
+      [
+        {title: "pick nose", description: "insert finger into left nostril, then right."},
+        {title: "play vieogames", description: "pick up thenm sticks!"}
+      ]};
+  },
+  addData: function(task) {
+    var tasks = this.state.data;
+    tasks.push(task);
+    this.setState({data: tasks});
+  },
+  render: function() {
+    return (
+      <div className="todoBox">
+        <TodoForm addDataz={this.addData} />
+        <TaskList taskData={this.state.data} />
+      </div>
+    );
+  }
+});
+
+var TodoForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var title = React.findDOMNode(this.refs.title).value.trim();
+    var description = React.findDOMNode(this.refs.description).value.trim();
+    if (!title || !description) {
+      alert("Both fields must be filled in!")
+      React.findDOMNode(this.refs.title).value = title;
+      React.findDOMNode(this.refs.description).value = description;
+      return;
+    }
+    this.props.addDataz({title: title, description: description});
+    React.findDOMNode(this.refs.title).value="";
+    React.findDOMNode(this.refs.description).value="";
+  },
+  render: function() {
+    return (
+      <form className="todoForm" onSubmit={this.handleSubmit}>
+        <input type="text" ref="title" placeholder="Enter Task Name" />
+        <input type="text" ref="description" placeholder="Describe Task"/>
+        <input type="submit" value="Add Task" />
+      </form>
+    );
+  }
+});
+
+var TaskList = React.createClass({
+  render: function() {
+    return(
+      <div>
+        {this.props.taskData}
+      </div>
+    );
+  }
+});
+
+React.render(
+  <TodoBox />,
+  document.getElementById('todoBox')
+);
